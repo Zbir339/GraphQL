7b3280f306f2f6603627dd105540700b464d5188
Zbir <aminezbir09@gmail.com>
2025-04-06 12:56:17 +0100

    Add the last edge case for many look ups

--- src/test/java/com/graphQl/demo/repository/BookRepositoryTest.java
+++ src/test/java/com/graphQl/demo/repository/BookRepositoryTest.java
@@ -9,11 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,6 +27,22 @@ public class BookRepositoryTest {
 
     @Autowired
     private AuthorRepository authorRepository;
+
+    @Sql("/data.sql")
+    @Test @DisplayName("Geting all books data including author data")
+    void findAllShouldReturnThreeBooks(){
+        Random rand = new Random();
+        List<BookEntity> books  = bookRepository.findAll();
+        int number = rand.nextInt(2);
+        assertAll("Asserting that the book is not null with 3 books and no null values within",
+                ()->assertEquals(books.size(),3,"Should return Non Null value"),
+                ()->assertNotNull(books.get(number).getName(),"The Name of a random book should not be null"),
+                ()->assertNotNull(books.get(number).getAuthor(),"The author part should not be null")); // if we cant to make our test case more edgy a book can sometimes have no author
+        /*Super Critical point here */
+        /*  N + 1 Problem a huge critical one that'll make your db and back app suffer eventually    */
+
+    }
+
 
     @Sql("/data.sql")
     @Test @DisplayName("Saving a new Book in the table")
@@ -47,14 +63,14 @@ public class BookRepositoryTest {
                 ()->assertEquals(books.size(),4,"Should return Non Null value"),
                 ()->assertEquals(books.get(3).getId(),4,"Should have id 4 since it's the forth item"),
                 ()->assertEquals(books.get(3).getName(),"Harry Potter And the Phoenix","Should have identical Name"),
-                ()->assertEquals(books.get(0).getAuthor().getName(),"JK Watkins"));
+                ()->assertEquals(books.get(3).getAuthor().getName(),"JK Watkins"));
         /*Super Critical point here */
-        for(int i= 0; i< books.size();i++ ){
-            System.out.println("Book :" + i + " Author Name "+books.get(i).getAuthor().getName());
-        }
+        /*  N + 1 Problem a huge critical one that'll make your db and back app suffer eventually    */
+
+//        for(int i= 0; i< books.size();i++ ){
+//            System.out.println("Book :" + i + " Author Name "+books.get(i).getAuthor().getName());
+//        }
     }
-
-
 
 
     @Sql("/data.sql")
