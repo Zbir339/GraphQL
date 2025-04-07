eec4736fa2b405a265be54f1e76e910d00c294a4
Mohamed Amine <amine339@outlook.fr>
2025-04-07 17:50:31 +0100

    Add the test part of authors

--- src/test/java/com/graphQl/demo/repository/AuthorRepositoryTest.java
+++ src/test/java/com/graphQl/demo/repository/AuthorRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,15 +21,53 @@ class AuthorRepositoryTest {
     @Autowired
     AuthorRepository authorRepository;
 
+
+
+
     @Test @Sql("/data.sql")
     void getAllAuthorsReturnListOfAuthors(){
 
         List<AuthorEntity> authors = authorRepository.findAll();
-        assertAll("",
+        assertAll("Finding All authors in my db and check weither they are null or not",
                 ()->assertNotNull(authors),
                 ()->assertEquals(3,authors.size()),
                 ()->assertEquals("JK Watkins",authors.get(0).getName())
         );
+    }
+
+    @Test @Sql("/data.sql")
+    void getAuthorByIdShouldReturnAuthor(){
+
+        AuthorEntity author = authorRepository.findById(1).get();
+
+        assertAll("Check the existing of the author id 1",
+                ()->assertNotNull(author,"Should Return not null"),
+                ()->assertEquals(1,author.getId()),
+                ()->assertEquals("JK Watkins",author.getName())
+        );
+
+    }
+
+
+    @Test @Sql("/data.sql")
+    void createAuthorShouldBeIncrementedInTheAuthorList(){
+
+        AuthorEntity author = AuthorEntity.builder()
+                .name("Mohamed")
+                .bornDate(LocalDate.of(1654,4,25))
+                .build();
+
+        authorRepository.save(author);
+
+        List<AuthorEntity> authors = authorRepository.findAll();
+
+        assertAll("Check the insertion of new Author",
+                ()->assertNotNull(authors,"Should Return not null"),
+                ()->assertEquals(4,authors.size()),
+                ()->assertNotNull(authors.get(3),"Should Not be of type Null"),
+                ()->assertEquals("Mohamed",authors.get(3).getName())
+        );
+
     }
 
 }
